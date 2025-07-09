fb1e42e420ecbe00fb50d35606a518064fa5670a
Lucas G <lucasgarreto20@gmail.com>
2025-07-09 10:02:27 -0300

    feat: add navbar componests

---
+++ app/_components/navbar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Navbar = () => {
+  const pathname = usePathname();
+  return (
+    <nav className="flex justify-between border-b border-solid px-8 py-4">
+      {/* ESQUERDA */}
+      <div className="flex items-center gap-10">
+        <Image src="/logo.svg" width={173} height={39} alt="Finance Ai" />
+        <Link
+          href="/"
+          className={
+            pathname == "/" ? "font-bold text-primary" : "text-muted-foreground"
+          }
+        >
+          Dashboard
+        </Link>
+        <Link
+          href="/transactions"
+          className={
+            pathname == "/transactions"
+              ? "font-bold text-primary"
+              : "text-muted-foreground"
+          }
+        >
+          Transactions
+        </Link>
+        <Link
+          href="/subscription"
+          className={
+            pathname == "/subscription"
+              ? "font-bold text-primary"
+              : "text-muted-foreground"
+          }
+        >
+          Assinatura
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,18 +3,22 @@ import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { transactionsColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
+import Navbar from "../_components/navbar";
 
 const TransitionsPage = async () => {
   const transactions = await db.transaction.findMany({});
   return (
-    <div className="space-y-6 p-6">
-      {/* TITULO E BOTÃO */}
-      <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl font-bold">Transações</h1>
-        <AddTransactionButton />
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        {/* TITULO E BOTÃO */}
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-2xl font-bold">Transações</h1>
+          <AddTransactionButton />
+        </div>
+        <DataTable columns={transactionsColumns} data={transactions} />
       </div>
-      <DataTable columns={transactionsColumns} data={transactions} />
-    </div>
+    </>
   );
 };
 
