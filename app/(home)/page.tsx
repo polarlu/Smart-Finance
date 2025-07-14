2aa8792a6b20bc81198f6bd79393e05633aa0b18
Lucas G <lucasgarreto20@gmail.com>
2025-07-14 15:46:25 -0300

    feat: ecroll user area

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -28,15 +28,15 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6">
+      <div className="flex space-y-6 overflow-hidden p-6">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <div className="flex items-center gap-3">
             <TimeSelect />
           </div>
         </div>
-        <div className="grid grid-cols-[2fr,1fr] gap-6">
-          <div className="flex flex-col gap-6">
+        <div className="grid grid-cols-[2fr,1fr] gap-6 overflow-hidden">
+          <div className="flex flex-col gap-6 overflow-hidden">
             <SummaryCards month={month} {...dashboard} />
             <div className="grid grid-cols-3 grid-rows-1 gap-6">
               <TransactionsPieChart investimentsTotal={0} {...dashboard} />
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -7,6 +7,7 @@ import Navbar from "../_components/navbar";
 
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
 
 const TransitionsPage = async () => {
   const { userId } = await auth();
@@ -22,13 +23,15 @@ const TransitionsPage = async () => {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6">
+      <div className="space-y-6 overflow-hidden p-6">
         {/* TITULO E BOTÃO */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
-        <DataTable columns={transactionsColumns} data={transactions} />
+        <ScrollArea>
+          <DataTable columns={transactionsColumns} data={transactions} />
+        </ScrollArea>
       </div>
     </>
   );
