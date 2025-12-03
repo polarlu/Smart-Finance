0c99f7b52c3aa193f863e6f9ca9de8515c2147ae
Lucas G <lucasgarreto20@gmail.com>
2025-12-03 16:35:58 -0300

    fix(types): update category type to use keys from TRANSACTION_CATEGORY_LABELS

--- app/data/get-dashboard/types.ts
+++ app/data/get-dashboard/types.ts
@@ -1,11 +1,12 @@
-import { TransactionCategory, TransactionType } from "@prisma/client";
+import { TransactionType } from "@prisma/client";
+import { TRANSACTION_CATEGORY_LABELS } from "@/app/_constants/transactions";
 
 export type TransactionPercentagePerType = {
   [key in TransactionType]: number;
 };
 
 export interface TotalExpensePerCategory {
-  category: TransactionCategory;
+  category: keyof typeof TRANSACTION_CATEGORY_LABELS; // ✅ tipo correto
   totalAmount: number;
   percentageOfTotal: number;
 }
