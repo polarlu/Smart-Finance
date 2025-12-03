0122d79c3006e3ea470aeef230b9554496ef076d
Lucas G <lucasgarreto20@gmail.com>
2025-12-03 16:10:02 -0300

    fix(transactions): update category validation to ensure non-empty string

--- app/_actions/upsert-transactions/schema.ts
+++ app/_actions/upsert-transactions/schema.ts
@@ -1,15 +1,11 @@
-import {
-  TransactionCategory,
-  TransactionPaymentMethod,
-  TransactionType,
-} from "@prisma/client";
+import { TransactionPaymentMethod, TransactionType } from "@prisma/client";
 import z from "zod";
 
 export const upsertTransactionSchema = z.object({
   name: z.string().trim().min(1),
   amount: z.number().positive(),
   type: z.nativeEnum(TransactionType),
-  category: z.nativeEnum(TransactionCategory),
+  category: z.string().min(1, "Categoria é obrigatória"), // z.nativeEnum(TransactionCategory),
   paymentMethod: z.nativeEnum(TransactionPaymentMethod),
   date: z.date(),
 });
